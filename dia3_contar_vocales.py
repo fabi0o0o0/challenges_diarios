8231c84262d1297787ed2b2b2473c0c02d7bfe68
Fabiola <fabiespinoza4560@gmail.com>
2024-06-13 23:21:43 -0400

    tabla de multiplicar: challenge nro 2

---
+++ dia3_contar_vocales.py
@@ -0,0 +1,15 @@
+# Contar Vocales: Escribe un programa que cuente el número de vocales en una cadena dada.
+
+def EsVocal(cadena):
+    vocales = "aeiou"
+    cont = 0
+
+    for letra in cadena:
+        if letra in vocales:
+            cont += 1
+
+    return cont
+
+palabra = input("Ingrese una palabra: ").lower()
+numero_vocales = EsVocal(palabra)
+print(f"El número de vocales en la palabra es: {numero_vocales}")
